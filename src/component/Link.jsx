6fe1c530c892a64911bff3eb5073c700452543be
DaimSyed <daimshah784@gmail.com>
2021-06-24 15:07:46 +0500

    updated

--- src/component/Link.jsx
+++ src/component/Link.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 function Link({ href, className, children }) {
   function handler(e) {
+    if (e.ctrlKey || e.metaKey) return;
     e.preventDefault();
     window.history.pushState({}, "", href);
     const navEvent = new PopStateEvent("popstate");
